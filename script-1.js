13715cfb3e1ef9c1e119ec21b2269c7363f6eb03
Felix Polo <brunolok@gmail.com>
2024-05-13 11:49:04 -0300

    se termino el segundo trabajo de la estructura repetitiva

---
+++ script-1.js
@@ -0,0 +1,7 @@
+let numerales = parseInt(prompt('ingre la cantidad de filas que quiere hacer el triengulo '));
+let triangulo = '';
+
+for (let i = 0; i <= numerales ; i++) {
+    triangulo += '#';
+    console.log(triangulo);
+}
